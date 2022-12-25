a57dc15920b48c9c8cb50ca62580d7e192a1ab8b
the_electro_bros <chinmaytanmay@outlook.com>
2022-12-25 21:18:56 +0000

    functions and includes work after much pain

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ outstack = []
 vars = []
 data = []
 funcs = []
-fprog = [[""]*50]*50
+fprog = [[""]*50 for _ in range(50)] 
 debug = False
-g = -1
-z = -3
+g = 0
+z = 0
 def getVar(name):
   if(name in vars):
     return data[vars.index(name)]
@@ -28,18 +28,21 @@ def pFunc(file):
   for i in range(len(fdata) - 1):
     #print(str(g)+","+str(z))
     #print(fprog)
-    if('\n' in fdata[i]):
-      z+= 1;
+    
     if("{" in fdata[i]):
-      z = 0;
-      g += 1
+      #z = 0
+      #g += 1
       funcs.append(fdata[i].strip('{').strip("\n").strip("func").strip('').strip('{').strip(' '))
-    #elif("}" in fdata[i]):
-      
+    elif("}" in fdata[i]):
+     # z = 0 
+      g+=1
     elif ("//" not in fdata[i]):
-      print(str(g)+","+str(z))
+      z+=1
+      #print(str(g)+","+str(z))
       try:
-        fprog[g][z] = str(fprog[g][z]) + fdata[i].strip('\n')
+       # print("GOOFY AHHH: "+fdata[i].strip('\n'))
+        fprog[g][z] =  fdata[i].strip('\n')
+        #print("Got: "+fprog[g][z])
       except:
         print("ERROR AT: ("+str(g)+","+str(z)+")")
   #print(funcs)
@@ -98,6 +101,7 @@ def parse(fileName):
       elif(lines[locY].strip(' ').strip('\n').split(" ")[0] in funcs):
         exc(funcs.index(lines[locY].strip(' ').split(" ")[0].strip('\n')))
         locY += 1
+       
         # do the function idk lamo
       elif("let" in lines[locY]):
         v = lines[locY].strip(' ').split(" ")
@@ -109,7 +113,8 @@ def parse(fileName):
         print(lines[locY].strip(' ').split(" "))
         break
 def exc(ind):
-  s = 0
+  global fprog
+  s = -3
   #print("IND: "+str(ind))
   d = open("tmp.gapl","w")
   with open("tmp.gapl","a") as g:
@@ -119,9 +124,12 @@ def exc(ind):
     g.write("entry{\n")
     while(True):
       try:
+        #print("AT: "+fprog[ind][s] +", I = "+str(ind)+", s="+str(s),)
         g.write(str(fprog[ind][s]).strip(''))
         g.write('\n');
         s+= 1;
+        if(s == 50):
+          break
       except Exception as e:
         #print("EEEE: "+str(s)+str(e))
         break
